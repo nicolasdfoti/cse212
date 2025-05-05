2c4363f079c2ecefe771a837bc15b062020cd6c1
Nicolás Foti <nicolasdfoti@gmail.com>
2025-05-05 14:29:59 -0300

    Practicing in Sand Box

--- sandbox/sandbox_project/Program.cs
+++ sandbox/sandbox_project/Program.cs
@@ -10,4 +10,14 @@ public class Program
 
         Console.WriteLine("Hello Sandbox World!");
     }
+
+    public static void RotateListRight(List<int> data, int amount)
+    {
+        List<int> lastElements = new List<int>();
+
+        for (int i = data.Count - amount; i < data.Count; i++) 
+        {
+            
+        }
+    }
 }
