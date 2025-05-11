1f5ffce705565aca4c035485c3229598d90f784a
Nicolás Foti <nicolasdfoti@gmail.com>
2025-05-10 22:09:26 -0300

    Finished 02 code assignments

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,23 +6,33 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: I wrote five problems, each one with a different value and priority.
+    // Expected Result: I expect the highest priority item to dequeue from the priorityQueue first (and if there are more than one PriorityItems with the same priority, the first one goes away)
+    // Defect(s) Found: None.
     public void TestPriorityQueue_1()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+
+        priorityQueue.Enqueue("problem1", 1);
+        priorityQueue.Enqueue("problem2", 4);
+        priorityQueue.Enqueue("problem3", 2);
+        priorityQueue.Enqueue("problem4", 4);
+        priorityQueue.Enqueue("problem5", 3);
+
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: I write four problems. Each one with values and priority numbers
+    // Expected Result: I expect problem 4 to dequeue first
+    // Defect(s) Found: None!
     public void TestPriorityQueue_2()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        
+        priorityQueue.Enqueue("problem1", 1);
+        priorityQueue.Enqueue("problem2", 2);
+        priorityQueue.Enqueue("problem3", 3);
+        priorityQueue.Enqueue("problem4", 4);
     }
 
     // Add more test cases as needed below.
