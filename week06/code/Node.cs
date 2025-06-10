da0285512535360a9246f4ce378a6c6fb3da13f8
Nicolás Foti <nicolasdfoti@gmail.com>
2025-06-09 23:24:23 -0300

    Finished Week06 Individual Activity

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography.X509Certificates;
+
 public class Node
 {
     public int Data { get; set; }
@@ -13,6 +15,11 @@ public class Node
     {
         // TODO Start Problem 1
 
+        if (value == Data)
+        {
+            return;
+        }
+
         if (value < Data)
         {
             // Insert to the left
@@ -21,6 +28,7 @@ public class Node
             else
                 Left.Insert(value);
         }
+
         else
         {
             // Insert to the right
@@ -34,12 +42,84 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+
+        if (value == Data)
+        {
+            return true;
+        }
+
+        else
+        {
+            if (value > Data)
+            {
+                if (Right == null)
+                {
+                    return false;
+                }
+
+                else
+                {
+                    if (Right.Contains(value))
+                    {
+                        return true;
+                    }
+
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            else
+            {
+                if (Left == null)
+                {
+                    return false;
+                }
+
+                else
+                {
+                    if (Left.Contains(value))
+                    {
+                        return true;
+                    }
+
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        // return 0; Replace this line with the correct return statement(s)
+
+        if (Right == null && Left == null)
+        {
+            return 1;
+        }
+
+        else
+        {
+            int rightHeight = 0;
+            int leftHeight = 0;
+
+            if (Left != null)
+            {
+                leftHeight = Left.GetHeight() + 1;
+            }
+
+            if (Right != null)
+            {
+                rightHeight = Right.GetHeight() + 1;
+            }
+
+            return Math.Max(rightHeight, leftHeight);
+        }
     }
 }
